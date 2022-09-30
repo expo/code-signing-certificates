17e0bcb5ef953c7a3dd508c6e574f76f4b115fab
Will Schurman <wschurman@gmail.com>
2022-09-30 15:20:36 -0700

    fix: add code to be robust against server clock skew

--- src/main.ts
+++ src/main.ts
@@ -325,8 +325,9 @@ export function generateDevelopmentCertificateFromCSR(
   // set certificate subject attrs from CSR
   certificate.setSubject(csr.subject.attributes);
 
-  // 30 day validity
+  // 30 day validity into the future, 5 days in the past just in case of clock skew at callsite
   certificate.validity.notBefore = new Date();
+  certificate.validity.notBefore.setDate(certificate.validity.notBefore.getDate() - 5);
   certificate.validity.notAfter = new Date();
   certificate.validity.notAfter.setDate(certificate.validity.notBefore.getDate() + 30);
 

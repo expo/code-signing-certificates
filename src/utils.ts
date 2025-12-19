fbcbad9b41c2dd205882c33db0262472db1f72c4
Phil Pluckthun <phil@kitten.sh>
2025-12-19 10:35:09 +0000

    Drop nullthrows

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,12 @@
-import nullthrows from 'nullthrows';
+import assert from 'assert';
 
 // a hexString is considered negative if its most significant bit is 1
 // because serial numbers use ones' complement notation
 // this RFC in section 4.1.2.2 requires serial numbers to be positive
 // http://www.ietf.org/rfc/rfc5280.txt
 export function toPositiveHex(hexString: string): string {
-  let mostSignificantHexAsInt = parseInt(nullthrows(hexString[0]), 16);
+  assert(hexString[0] != null);
+  let mostSignificantHexAsInt = parseInt(hexString[0], 16);
   if (mostSignificantHexAsInt < 8) {
     return hexString;
   }
